e07c92eb112eeeaea0f1aa430165a2b23f6b8276
muthupandi053 <137278500+muthupandi053@users.noreply.github.com>
2023-06-21 16:38:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
